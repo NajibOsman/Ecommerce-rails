56fedf1f95f7fa29d366a67d1c4e597533b98c02
NajibOsman <machidydemajka38@gmail.com>
2017-08-21 08:55:11 +0300

    Added the product account and order id  for the database

--- db/migrate/20170820183249_create_orders.rb
+++ db/migrate/20170820183249_create_orders.rb
@@ -2,7 +2,9 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.string :status
-      t.decimal :total_price
+      t.decimal :total
+
+      t.timestamps
     end
   end
 end
---
+++ db/migrate/20170820201245_add_order_items_to_products.rb
@@ -0,0 +1,5 @@
+class AddOrderItemsToProducts < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :products, :order_items, foreign_key: true
+  end
+end
